ae05c7863eba201bfd3a71c55e8b077531eea39d
Raluca Ioniță <raluca.ionita04@gmail.com>
2020-05-03 13:43:39 +0300

    Corrected the Startup.cs file. Now it's working.

--- MoodyTunes/Startup.cs
+++ MoodyTunes/Startup.cs
@@ -30,22 +30,9 @@ namespace MoodyTunes
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
@@ -59,5 +46,26 @@ namespace MoodyTunes
             services.AddTransient<ISongRepository, SongRepository>();
             services.AddTransient<IUserRepository, UserRepository>();
         }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseCors(builder => builder.AllowAnyOrigin()
+                                        .AllowAnyMethod()
+                                        .AllowAnyHeader()
+                                        .AllowCredentials());
+            app.UseMvc();
+
+
+        }
+
     }
 }
